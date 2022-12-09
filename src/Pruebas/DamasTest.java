29317a0877e1ccaac7caf029fb3c8756e1a4d854
JuanPabloDaza <jpablo.daza@hotmail.com>
2022-12-09 12:39:03 -0500

    Pruebas

---
+++ src/Pruebas/DamasTest.java
@@ -0,0 +1,14 @@
+package Pruebas;
+import static org.junit.Assert.*;
+import com.sun.tools.javac.Main;
+import domain.Damas;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class DamasTest {
+    @Test
+    public void deberiaRealizarElMovimiento(){
+        Damas prueba = new Damas();
+    }
+}
